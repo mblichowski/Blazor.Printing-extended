7485613ea164e044f7ea715ee9553a0297a15ed1
Marcin Blichowski <74534746+mblichowski@users.noreply.github.com>
2023-08-02 21:52:19 +0200

    print options added

---
+++ samples/Append.Blazor.Printing.Wasm/Examples/ExtendendOptions.razor
@@ -0,0 +1,50 @@
+﻿@inject IPrintingService PrintingService
+<Example Name="@this.GetType().Name">
+    <Documentation>
+        <h3>HTML with extended options</h3>
+        <p>Print a HTML element with a specific Id and additional options.</p>
+        <p>@_options.ToString()</p>
+    </Documentation>
+    <Code>
+        <form id="printable-form">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label text-primary" for="exampleCheck1">Check me out</label>
+            </div>
+            <div id="@_ignoredElementId1">
+                <p>This content will not be printed</p>
+            </div>
+            <div id="@_ignoredElementId2">
+                <p>This content will not be printed too</p>
+            </div>
+        </form>
+
+        <button @onclick="@(_ => PrintingService.Print(_options))">
+            Print Form
+        </button>
+    </Code>
+</Example>
+
+@code {
+    private static string _ignoredElementId1 = "ignored-element-1";
+    private static string _ignoredElementId2 = "ignored-element-2";
+
+    private readonly PrintOptions _options = new()
+    {
+        Printable = "printable-form",
+        Type = PrintType.Html,
+        DocumentTitle = "Custom document title",
+        IgnoreElements = new[] { _ignoredElementId1, _ignoredElementId2 },
+        Style = "color: red !important;",        
+    };
+
+}
--- source/Append.Blazor.Printing/PrintOptionsAdapter.cs
+++ source/Append.Blazor.Printing/PrintOptionsAdapter.cs
@@ -1,26 +1,37 @@
-﻿using System;
-
-namespace Append.Blazor.Printing
+﻿namespace Append.Blazor.Printing
 {
-    /// <summary>
-    /// Adapts the <see cref="PrintOptions"/> to the JavaScript version.
-    /// </summary>
-    internal record PrintOptionsAdapter
-    {
-        public string Printable { get; init; }
-        public string Type { get; init; }
-        public bool ShowModal { get; init; }
-        public string ModalMessage { get; init; } = "Retrieving Document...";
-        public bool? Base64 { get; set; }
-        public string TargetStyles { get; set; } = "['*']";
+	/// <summary>
+	/// Adapts the <see cref="PrintOptions"/> to the JavaScript version.
+	/// </summary>
+	internal record PrintOptionsAdapter
+	{
+		public string Printable { get; init; }
+		public string Type { get; init; }
+		public bool ShowModal { get; init; }
+		public string ModalMessage { get; init; } = "Retrieving Document...";
+		public bool? Base64 { get; set; }
+		public bool ScanStyles { get; set; }
+		public string Style { get; set; }
+		public string TargetStyles { get; set; }
+		public string[] IgnoreElements { get; set; }
+		public string DocumentTitle { get; set; }
+		public string OnLoadingStart { get; set; }
+		public string OnLoadingEnd { get; set; }
 
-        public PrintOptionsAdapter(PrintOptions options)
-        {
-            Printable = options.Printable;
-            Type = options.Type.ToPrintJsString();
-            ShowModal = options.ShowModal;
-            ModalMessage = options.ModalMessage;
-            Base64 = options.Base64 == true ? true : null;
-        }
-    }
+		public PrintOptionsAdapter(PrintOptions options)
+		{
+			Printable = options.Printable;
+			Type = options.Type.ToPrintJsString();
+			ShowModal = options.ShowModal;
+			ModalMessage = options.ModalMessage;
+			Base64 = options.Base64 == true ? true : null;
+			Style = options.Style;
+			TargetStyles = options.TargetStyles;
+			ScanStyles = options.ScanStyles;
+			IgnoreElements = options.IgnoreElements;
+			DocumentTitle = options.DocumentTitle;
+			OnLoadingStart = options.OnLoadingStart;
+			OnLoadingEnd = options.OnLoadingEnd;
+		}
+	}
 }
