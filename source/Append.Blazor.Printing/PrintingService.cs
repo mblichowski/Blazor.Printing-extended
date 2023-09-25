a3af13183f1832607b7df2130458592c5c9e761e
Marcin Blichowski <74534746+mblichowski@users.noreply.github.com>
2023-09-25 17:18:33 +0200

    module import path bugfix

--- source/Append.Blazor.Printing/PrintingService.cs
+++ source/Append.Blazor.Printing/PrintingService.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.JSInterop;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Append.Blazor.Printing
@@ -33,12 +27,12 @@ namespace Append.Blazor.Printing
         }
         public Task Print(string printable, bool showModal, PrintType printType = PrintType.Pdf)
         {
-            return Print(new PrintOptions(printable) { ShowModal= showModal, Type = printType });
+            return Print(new PrintOptions(printable) { ShowModal = showModal, Type = printType });
         }
 
         internal async ValueTask ImportModule()
         {
-            module = await jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Append.Blazor.Printing/scripts.js");
+            module = await jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Append.Blazor.Printing-extended/scripts.js");
         }
     }
 }
