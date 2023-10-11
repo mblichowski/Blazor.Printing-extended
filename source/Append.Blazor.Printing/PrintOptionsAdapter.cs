9b170be732e33f91dbfe3b45e849fcc56195c8fc
Marcin Blichowski <74534746+mblichowski@users.noreply.github.com>
2023-10-11 14:29:38 +0200

    minor formatting update

--- source/Append.Blazor.Printing/PrintOptionsAdapter.cs
+++ source/Append.Blazor.Printing/PrintOptionsAdapter.cs
@@ -1,44 +1,43 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Append.Blazor.Printing
+namespace Append.Blazor.Printing;
+
+/// <summary>
+/// Adapts the <see cref="PrintOptions"/> to the JavaScript version.
+/// </summary>
+internal record PrintOptionsAdapter
 {
-    /// <summary>
-    /// Adapts the <see cref="PrintOptions"/> to the JavaScript version.
-    /// </summary>
-    internal record PrintOptionsAdapter
+    public string Printable { get; init; }
+    public string Type { get; init; }
+    public bool ShowModal { get; init; }
+    public string ModalMessage { get; init; } = "Retrieving Document...";
+    public bool? Base64 { get; set; }
+    public bool ScanStyles { get; set; }
+    public string Style { get; set; }
+    public string TargetStyles { get; set; }
+    public string[] IgnoreElements { get; set; }
+    public string DocumentTitle { get; set; }
+    public string OnLoadingStart { get; set; }
+    public string OnLoadingEnd { get; set; }
+    [JsonPropertyName("font_size")]
+    public string FontSize { get; set; } = "12px";
+
+    public PrintOptionsAdapter(PrintOptions options)
     {
-        public string Printable { get; init; }
-        public string Type { get; init; }
-        public bool ShowModal { get; init; }
-        public string ModalMessage { get; init; } = "Retrieving Document...";
-        public bool? Base64 { get; set; }
-        public bool ScanStyles { get; set; }
-        public string Style { get; set; }
-        public string TargetStyles { get; set; }
-        public string[] IgnoreElements { get; set; }
-        public string DocumentTitle { get; set; }
-        public string OnLoadingStart { get; set; }
-        public string OnLoadingEnd { get; set; }
-        [JsonPropertyName("font_size")]
-        public string FontSize { get; set; } = "12px";
+        Printable = options.Printable;
+        Type = options.Type.ToPrintJsString();
+        ShowModal = options.ShowModal;
+        ModalMessage = options.ModalMessage;
+        Base64 = options.Base64 == true ? true : null;
+        Style = options.Style;
+        TargetStyles = options.TargetStyles;
+        ScanStyles = options.ScanStyles;
+        IgnoreElements = options.IgnoreElements;
+        DocumentTitle = options.DocumentTitle;
+        OnLoadingStart = options.OnLoadingStart;
+        OnLoadingEnd = options.OnLoadingEnd;
 
-        public PrintOptionsAdapter(PrintOptions options)
-        {
-            Printable = options.Printable;
-            Type = options.Type.ToPrintJsString();
-            ShowModal = options.ShowModal;
-            ModalMessage = options.ModalMessage;
-            Base64 = options.Base64 == true ? true : null;
-            Style = options.Style;
-            TargetStyles = options.TargetStyles;
-            ScanStyles = options.ScanStyles;
-            IgnoreElements = options.IgnoreElements;
-            DocumentTitle = options.DocumentTitle;
-            OnLoadingStart = options.OnLoadingStart;
-            OnLoadingEnd = options.OnLoadingEnd;
-
-            if (options.OverrideDefaultFontSize)
-                FontSize = "";
-        }
+        if (options.OverrideDefaultFontSize)
+            FontSize = "";
     }
 }
